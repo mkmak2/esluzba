1e4bcab71adc0cd2e8725345ba786185be7636df
mkmak <mkmak.code@gmail.com>
2024-06-20 12:34:44 +0200

    Dockerize app

--- server/index.ts
+++ server/index.ts
@@ -7,7 +7,7 @@ import cors from 'cors'
 dotenv.config();
 
 const app: Express = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 const corsOptions = {
   origin: 'http://localhost:3000',
